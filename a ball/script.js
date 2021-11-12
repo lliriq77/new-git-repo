89a54885382d3e11cb339cc602f6956646a1a483
Kirill Tikhonov <lliriq@me.com>
2021-11-12 21:08:38 +0300

    Add function

--- a ball/script.js
+++ a ball/script.js
@@ -1,2 +1,6 @@
-ball.style.top = `${field.clientHeight / 2 - ball.offsetHeight / 2}px`;
-ball.style.left = `${field.clientWidth / 2 - ball.offsetWidth / 2}px`;
+function centerBall(ball, field) {
+
+    ball.style.top = `${field.clientHeight / 2 - ball.offsetHeight / 2}px`;
+    ball.style.left = `${field.clientWidth / 2 - ball.offsetWidth / 2}px`;
+}
+centerBall(ball, field);
